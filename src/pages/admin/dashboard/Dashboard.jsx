c99ace23fd8fc47b797288281982aa981549dcf3
SmartHagar <smarthagar@gmail.com>
2023-02-15 20:47:48 +0900

    menambah saldo kantin pada bendahara

--- src/pages/admin/dashboard/Dashboard.jsx
+++ src/pages/admin/dashboard/Dashboard.jsx
@@ -6,15 +6,24 @@ import showRupiah from "../../../services/rupiah";
 import useSaldo from "../../../store/api/saldo";
 
 import bg3 from "../../../assets/gambar/bg-3.jpeg";
+import useSaldoKantin from "../../../store/api/saldo-kantin";
 
 const Dashboard = () => {
   const [isLoading, setIsLoading] = useState(false);
   const { setSaldo, dtSaldo, pemasukanTerakhir, pengeluaranTerakhir } =
     useSaldo();
+  const {
+    setSaldoKantin,
+    dtSaldoKantin,
+    pemasukanTerakhirKantin,
+    pengeluaranTerakhirKantin,
+  } = useSaldoKantin();
+
   useEffect(() => {
     setIsLoading(true);
     const fetch = async () => {
       await setSaldo();
+      await setSaldoKantin();
       setIsLoading(false);
     };
     fetch();
@@ -22,19 +31,19 @@ const Dashboard = () => {
 
   return (
     <div
-      className="h-[92vh] -mt-6 -mx-4"
+      className="min-h-[92vh] -mt-6 -mx-4 flex"
       style={{
         backgroundImage: `url(${bg3})`,
       }}
     >
-      {console.log(dtSaldo)}
-      <div className="flex justify-center items-center h-full w-full bg-white/50">
+      <div className="flex justify-center items-center min-h-full w-full bg-white/50">
         <div className="w-[30rem] h-4/5 flex items-center px-8 py-4 bg-white/95 rounded-lg shadow-sm shadow-biru">
           <div className="w-full">
             {isLoading ? (
               <SkletonCard />
             ) : (
               <div>
+                <h1 className="text-center text-xl text-biru">FST</h1>
                 <div className="mb-14">
                   <h1 className="text-center text-biru text-2xl font-bold">
                     Saldo Terakhir
@@ -62,6 +71,50 @@ const Dashboard = () => {
                     </p>
                     <p className="text-merah font-Charmonman text-xl">
                       {showRupiah(parseInt(pengeluaranTerakhir?.jumlah))}
+                    </p>
+                  </div>
+                </div>
+              </div>
+            )}
+          </div>
+        </div>
+      </div>
+      {/* saldo kantin */}
+      <div className="flex justify-center items-center min-h-full w-full bg-white/50">
+        <div className="w-[30rem] h-4/5 flex items-center px-8 py-4 bg-white/95 rounded-lg shadow-sm shadow-biru">
+          <div className="w-full">
+            {isLoading ? (
+              <SkletonCard />
+            ) : (
+              <div>
+                <h1 className="text-center text-xl text-biru">Kantin</h1>
+                <div className="mb-14">
+                  <h1 className="text-center text-biru text-2xl font-bold">
+                    Saldo Terakhir
+                  </h1>
+                  <p className="text-merah text-center font-Charmonman text-2xl">
+                    {dtSaldoKantin.saldo && showRupiah(dtSaldoKantin.saldo)}
+                  </p>
+                </div>
+                <div className="my-6">
+                  <h2 className="text-biru text-xl">Pemasukan Terakhir</h2>
+                  <div className="border p-2 rounded-lg">
+                    <p className="text-gray-600 text-xl">
+                      {pemasukanTerakhirKantin?.ket}
+                    </p>
+                    <p className="text-merah font-Charmonman text-xl">
+                      {showRupiah(parseInt(pemasukanTerakhirKantin?.jumlah))}
+                    </p>
+                  </div>
+                </div>
+                <div className="my-6">
+                  <h2 className="text-biru text-xl">Pengeluaran Terakhir</h2>
+                  <div className="border p-2 rounded-lg">
+                    <p className="text-gray-600 text-xl">
+                      {pengeluaranTerakhirKantin?.ket}
+                    </p>
+                    <p className="text-merah font-Charmonman text-xl">
+                      {showRupiah(parseInt(pengeluaranTerakhirKantin?.jumlah))}
                     </p>
                   </div>
                 </div>
